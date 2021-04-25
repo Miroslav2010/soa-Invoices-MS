1ee0563fbf8de6feb0dd8bb43ff99ba02fa5e16c
teodorpenevski <67396709+teodorpenevski@users.noreply.github.com>
2021-04-25 22:25:56 +0200

    implemented marshmallow and refactored methods

--- app.py
+++ app.py
@@ -1,60 +1,116 @@
+from datetime import date
+import urllib3
+from functools import wraps
+from flask import request, abort
 import connexion
-import datetime
-# dummy reference for migrations only
-
-
+import jwt
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from invoice import *
 
-def get_test1(test1_id):
-    return {'id': 1, 'name': 'name', 'entered_id': test1_id}
+# dummy reference for migrations only
 
 
+invoice_schema = InvoiceSchema()
+invoices_schema = InvoiceSchema(many=True)
+JWT_SECRET = 'INVOICES MS SECRET'
+JWT_LIFETIME_SECONDS = 600000
+
+user_ms_url = 'http://localhost:5001/api'
+payments_ms_url = 'http://localhost:5002/api'
+inventory_ms_url = 'http://localhost:5003/api'
+statistics_ms_url = 'http://localhost:5004/api'
+
+http = urllib3.PoolManager()
+
+
+def has_role(arg):
+    def has_role_inner(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            try:
+                headers = request.headers
+                if 'AUTHORIZATION' in headers:
+                    token = headers['AUTHORIZATION'].split(' ')[1]
+                    decoded_token = decode_token(token)
+                    if 'admin' in decoded_token['roles']:
+                        return fn(*args, **kwargs)
+                    for role in arg:
+                        if role in decoded_token['roles']:
+                            return fn(*args, **kwargs)
+                    abort(401)
+                return fn(*args, **kwargs)
+            except Exception as e:
+                abort(401)
+
+        return decorated_view
+
+    return has_role_inner
+
+
+@has_role(['admin', 'shopping_cart'])
 def create_invoice(invoice_body):
-    # Get User Details from User microservice : TODO
+    # Get User Details from User microservice
+    user = http.request('GET', f"{user_ms_url}/details/{invoice_body['user_id']}")
 
-    # Get Transaction details from Payments microservice : TODO
+    # Get Transaction details from Payments microservice
+    transaction = http.request('GET', f"{payments_ms_url}/transactions/{invoice_body['transaction_id']}")
 
-    # Get Items from Inventory microservice : TODO
+    # Get Items from Inventory microservice
+    items = []
+    for item in invoice_body['item_list']:
+        if item.type == 'Rent':
+            new_item = http.request('GET', f"{inventory_ms_url}/get/product_rent/{item.id}")
+            items.append(new_item)
+        elif item.type == 'Buy':
+            new_item = http.request('GET', f"{inventory_ms_url}/get/product_buy/{item.id}")
+            items.append(new_item)
 
-    new_invoice = Invoice(id=1, name='Petko', surname='Petkovski',
-                          username='Petko123', country='Macedonia',
-                          city='Debar', postal_code=1250, address='Ulica br.11/11',
-                          price=69.99, currency='MKD', date=datetime.date(2021, 4, 8))
+    new_invoice = Invoice(name=user.name, surname=user.surname,
+                          username=user.username, country=user.country,
+                          city=user.city, postal_code=user.postal_code, address=user.address,
+                          price=transaction.amount, date=date.today(), items=items)
+
+    # new_invoice = Invoice(id=1, name='Petko', surname='Petkovski',
+    #                       user_id=1, country='Macedonia',
+    #                       city='Debar', postal_code=1250, address='Ulica br.11/11',
+    #                       price=69.99, date=date.today())
     db.session.add(new_invoice)
     db.session.commit()
 
     # Notify discount microservice if a coupon is bought : TODO
 
-    # Notify statistics microservice : TODO
+    # Notify statistics microservice : TODO Do this for each item :
+    # http.request('POST', f"{statistics_ms_url}/invoice/", data = {'user_id': invoice_body['user_id'], 'type_of_product': item.type, 'quantity': item.quantity})
 
 
+@has_role(['admin'])
 def get_invoice_by_id(invoice_id):
     found_invoice = db.session.query(Invoice).filter_by(id=invoice_id).first()
     if found_invoice:
-        return {'id': found_invoice.id, 'name': found_invoice.name, 'surname': found_invoice.surname,
-                'username': found_invoice.username, 'country': found_invoice.country, 'city': found_invoice.city,
-                'postal_code': found_invoice.postal_code, 'address': found_invoice.address,
-                'price': found_invoice.price, 'currency': found_invoice.currency, 'date': found_invoice.date}
+        return result_invoice(found_invoice)
     else:
         return {'error': '{} not found'.format(invoice_id)}, 404
 
 
+@has_role(['admin'])
 def get_invoice_by_user(user_id):
-    user_invoices = db.session.query(Invoice).filter_by(user_id)
+    user_invoices = db.session.query(Invoice).filter_by(user_id).all()
     if user_invoices:
-        return user_invoices
+        return result_invoice(user_invoices)
     else:
         return {'error': 'No invoices for user {}'.format(user_id)}, 404
 
 
+@has_role(['admin'])
 def get_all():
     all_invoices = db.session.query(Invoice).all()
     if all_invoices:
-        return all_invoices
+        return result_invoice(all_invoices)
     else:
         return {'error': 'No invoices found'}, 404
+
 
 # def person_add(person_body):
 #     new_person = Person(name=person_body['name'], surname=person_body['surname'])
@@ -68,6 +124,10 @@ def get_all():
 #         return {'id': found_person.id, 'name': found_person.name, 'surname': found_person.surname}
 #     else:
 #         return {'error': '{} not found'.format(person_name)}, 404
+
+
+def decode_token(token):
+    return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
 
 
 connexion_app = connexion.App(__name__, specification_dir="./")
--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import uuid
 
 
 class Invoice(db.Model):
+    db.__tablename__ = 'invoice'
     generatedId = uuid.uuid1()
     id = db.Column(db.BigInteger, default=generatedId.int, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -13,14 +14,16 @@ class Invoice(db.Model):
     postal_code = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    currency = db.Column(db.String, nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    date = db.Column(db.DATE, nullable=False)
+    items = db.relationship("Item")
 
 
 class Item(db.Model):
+    db.__tablename__ = 'item'
     id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String, nullable=False)
     type = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=True)
     quantity = db.Column(db.Integer, nullable=True)
     duration = db.Column(db.Integer, nullable=True)
+    invoice_id = db.Column(db.BigInteger, db.ForeignKey('invoice.id'))
